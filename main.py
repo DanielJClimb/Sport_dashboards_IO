a4da6100c09ae2df6070be0aa3100d32271797f1
Kuba Lewandowski <134650399+KubaLewandowski01@users.noreply.github.com>
2025-01-08 21:47:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,7 @@ players = [p for p in players if p == p]  # removing nan values
 
 # function for pass visualization
 def pass_visualization(player):
-    passes = event[(event.player == player) & (event.type == "Pass") & (event.pass_outcome.isna())]
-    # print(passes.pass_outcome)
-    # print(passes.pass_type)
-    # print(passes)
+    passes = event[(event.player == player) & (event.type == "Pass")]
     # creating pitch
     pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black')
     fig, ax = pitch.draw(figsize=(16, 11))
@@ -40,9 +37,21 @@ def pass_visualization(player):
         passes[['x', 'y']] = passes['location'].apply(pd.Series)
         passes[['pass_end_x', 'pass_end_y']] = passes['pass_end_location'].apply(pd.Series)
 
+        passes_good = passes[passes.pass_outcome.isna()]
+        passes_assist = passes[passes.pass_goal_assist == True]
+        #pitch.text(5,5,len(passes),ax)
+        #pitch.text(5,10,len(passes_good),ax)
+        #pitch.text(5,15,len(passes_assist),ax)
+
         # drawing passes
         pitch.arrows(passes.x, passes.y, passes.pass_end_x, passes.pass_end_y,
+                     width=3, headwidth=8, headlength=5, color='red', ax=ax, zorder=2, label="Pass")
+        # drawing good passes
+        pitch.arrows(passes_good.x, passes_good.y, passes_good.pass_end_x, passes_good.pass_end_y,
                      width=3, headwidth=8, headlength=5, color='green', ax=ax, zorder=2, label="Pass")
+        # drawing assist passes
+        pitch.arrows(passes_assist.x, passes_assist.y, passes_assist.pass_end_x, passes_assist.pass_end_y,
+                     width=5, headwidth=8, headlength=5, color='black', ax=ax, zorder=2, label="Pass")
         return fig
 
 
