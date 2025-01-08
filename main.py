fa04aedf6e6a9d1612756b3f51d62cdb4c9313ac
DanielJClimb <danio2001.dj@gmail.com>
2025-01-08 11:50:11 +0100

    Uploading update
    Deleted plots which I considered to be not important

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def pass_visualization(player):
 
         # drawing passes
         pitch.arrows(passes.x, passes.y, passes.pass_end_x, passes.pass_end_y,
-                     width=3, headwidth=8, headlength=5, color='red', ax=ax, zorder=2, label="Pass")
+                     width=3, headwidth=8, headlength=5, color='green', ax=ax, zorder=2, label="Pass")
         return fig
 
 
@@ -65,90 +65,17 @@ def ball_recovery_visualization(player):
         return fig
 
 
-# function for dispossessed visualization
-def dispossessed_visualization(player):
-    player_d = event[
-        (event.player == player) & (event.type == "Dispossessed")]
-    # creating pitch
-    pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black')
-    fig, ax = pitch.draw(figsize=(16, 11))
-    ax_title = ax.set_title(f'{player} Dispossessed', fontsize=30)
-    fig.set_facecolor('white')
-    if len(player_d) == 0:
-        return fig
-
-    else:
-        player_d[['x', 'y']] = player_d['location'].apply(pd.Series)
-        # drawing ball dispossessed location
-        pitch.scatter(player_d.x, player_d.y, color='red', s=200, marker="o", ax=ax, zorder=2, label="Dispossessed")
-        return fig
 
 
-# function for won duels visualization
-def duel_visualization(player):
-    duels = event[(event.player == player) & (event.type == "Duel")]
-    # creating pitch
-    print(duels.duel_outcome)
-    print(duels.duel_type)
-    pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black')
-    fig, ax = pitch.draw(figsize=(16, 11))
-    ax_title = ax.set_title(f'{player} duels', fontsize=30)
-    fig.set_facecolor('white')
-    if len(duels) == 0:
-        return fig
-    else:
-        # pitch.text(5, 5, len(duels), ax)
-        # pitch.text(5, 10, duels.duel_outcome, ax)
-        duels[['x', 'y']] = duels['location'].apply(pd.Series)
-        # drawing duel locations
-        pitch.scatter(duels.x, duels.y, color='red', s=200, marker="o", ax=ax, zorder=2, label="Duel")
-        duels_won = duels[(duels.duel_type == "Tackle")
-                      & ((duels.duel_outcome == "Success In Play") | (duels.duel_outcome == "Won")
-                         | (duels.duel_outcome == "Success") | (duels.duel_outcome == "Success Out")
-                         | (duels.duel_outcome == "Success To Team"))]
-        # pitch.text(10, 5, len(duels_won), ax)
-        if len(duels_won) > 0:
-            duels_won[['x', 'y']] = duels_won['location'].apply(pd.Series)
-            pitch.scatter(duels_won.x, duels_won.y, color='green', s=200, marker="o", ax=ax, zorder=2, label="DuelWon")
-        return fig
-
-# function for shot visualization
-def shot_visualization(player):
-    player_shots = event[(event.player == player) & (event.type == "Shot")]
-    # creating pitch
-    pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black')
-    fig, ax = pitch.draw(figsize=(16, 11))
-    ax_title = ax.set_title(f'{player} shots', fontsize=30)
-    fig.set_facecolor('white')
-    if len(player_shots) == 0:
-        pitch.scatter(0, 0, color='red', s=200, marker="o", ax=ax, zorder=2, label="Shot")
-        return fig
-    else:
-        pitch.scatter(0, 0, color='blue', s=200, marker="o", ax=ax, zorder=2, label="Shot")
-        player_shots[['x', 'y']] = player_shots['location'].apply(pd.Series)
-        pitch.scatter(player_shots.x, player_shots.y, color='red', s=200, marker="o", ax=ax, zorder=2, label="Shot")
-        # player_shots[['pass_end_x', 'pass_end_y']] = player_shots['pass_end_location'].apply(pd.Series)
-
-        # drawing shots
-        # pitch.arrows(player_shots.x, player_shots.y, player_shots.pass_end_x, player_shots.pass_end_y,
-        #              width=3, headwidth=8, headlength=5, color='green', ax=ax, zorder=2, label="Pass")
-        return fig
 
 
 chosen_player = st.selectbox("Players:", players)  # players selectbox
-status = st.selectbox("Aspects :", ['Pass', 'Recovery', 'Duel', 'Dispossessed', 'Shot'])
+status = st.selectbox("Aspects :", ['Pass', 'Recovery'])
 
 if status == 'Pass':
     st.pyplot(pass_visualization(chosen_player))
 
-elif status == 'Dispossessed':
-    st.pyplot(dispossessed_visualization(chosen_player))
-elif status == 'Shot':
-    st.pyplot(shot_visualization(chosen_player))
 else:
-    if status == 'Recovery':
-        st.pyplot(ball_recovery_visualization(chosen_player))
-    else:
-        st.pyplot(duel_visualization(chosen_player))
+    st.pyplot(ball_recovery_visualization(chosen_player))
 
 # print(event)
