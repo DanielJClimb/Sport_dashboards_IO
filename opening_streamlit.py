56a9667741dacb576b0595e8d6c452f36d88eaee
Kubbikub <134650399+Kubbikub@users.noreply.github.com>
2025-01-17 19:29:37 +0100

    Update opening_streamlit.py

--- opening_streamlit.py
+++ opening_streamlit.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec  5 14:59:06 2024
-
-@author: danio
 """
 
 import os
 print('Please wait, while SCOS app is opening')
-os.system('streamlit run 'r'main.py')
-
+os.system('streamlit run 'r'titlepage.py')
