c63422926a23481528112730b19f07b313a96886
Kubbikub <134650399+Kubbikub@users.noreply.github.com>
2025-01-17 19:31:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ import streamlit as st
 from PIL import Image
 from matplotlib.backends.backend_pdf import PdfPages
 
-st.write ("data provided by")
-logofile = 'hudlstatsbomblogo.jpg' #setting required statsbomb logo 
-st.image(logofile)
-
 competitions = sb.competitions()  # getting data of all available competitions
 
 matches = sb.matches(competition_id=9, season_id=281)  # getting matches from Bundesliga season
@@ -36,7 +32,7 @@ def duel_pitch(ax, player):
     # creating pitch
     pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black', linewidth=1)
     pitch.draw(ax=ax, figsize=(16, 11))
-    ax_title = ax.set_title('Duels', fontsize=8)
+    ax_title = ax.set_title('Duels', fontsize=10)
     if len(duels) > 0:
         duels[['x', 'y']] = duels['location'].apply(pd.Series)
         # drawing duel locations
@@ -55,12 +51,8 @@ def duel_pitch(ax, player):
             duels_lost_dispossessed[['x', 'y']] = duels_lost_dispossessed['location'].apply(pd.Series)
             pitch.scatter(duels_lost_dispossessed.x, duels_lost_dispossessed.y, color='red', s=7, marker="o", ax=ax,
                           zorder=2, label="Duel lost or dispossessed")
-        # if len(dispossessed) > 0:
-        #     dispossessed[['x', 'y']] = dispossessed['location'].apply(pd.Series)
-        #     pitch.scatter(dispossessed.x, dispossessed.y, color='red', s=7, marker="o", ax=ax, zorder=2)
 
-    ax.legend(loc='lower center', ncol=3, fontsize=7, bbox_to_anchor=(0.5, -0.05))
-
+    ax.legend(loc='lower center', ncol=3, fontsize=8, bbox_to_anchor=(0.5, -0.07))
     return
 
 
@@ -70,7 +62,7 @@ def pass_pitch(ax, player):
     # creating pitch
     pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black', linewidth=1)
     pitch.draw(ax=ax, figsize=(16, 11))
-    ax_title = ax.set_title('Passes', fontsize=8)
+    ax_title = ax.set_title('Passes', fontsize=10)
     if len(passes) > 0:
         passes[['x', 'y']] = passes['location'].apply(pd.Series)
         passes[['pass_end_x', 'pass_end_y']] = passes['pass_end_location'].apply(pd.Series)
@@ -89,8 +81,7 @@ def pass_pitch(ax, player):
         pitch.arrows(passes_assist.x, passes_assist.y, passes_assist.pass_end_x, passes_assist.pass_end_y,
                      width=1.5, headwidth=4, headlength=3, color='green', ax=ax, zorder=2, label="Assist")
 
-    ax.legend(loc='lower center', ncol=3, fontsize=7, bbox_to_anchor=(0.5, -0.05))
-
+    ax.legend(loc='lower center', ncol=3, fontsize=8, bbox_to_anchor=(0.5, -0.07))
     return
 
 
@@ -100,11 +91,10 @@ def recovery_pitch(ax, player):
     # creating pitch
     pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black', linewidth=1)
     pitch.draw(ax=ax, figsize=(16, 16))
-    ax_title = ax.set_title('Ball recoveries', fontsize=8)
+    ax_title = ax.set_title('Ball recoveries', fontsize=10)
     if len(recoveries) > 0:
         recoveries[['x', 'y']] = recoveries['location'].apply(pd.Series)
         # drawing duel locations
-        # pitch.scatter(recoveries.x, recoveries.y, color='red', s=7, marker="o", ax=ax, zorder=2, label="Recovery")
         recoveries_won = recoveries[(recoveries.ball_recovery_recovery_failure.isna())]
         recoveries_lost = recoveries[(recoveries.ball_recovery_recovery_failure == "True")]
 
@@ -117,8 +107,7 @@ def recovery_pitch(ax, player):
             pitch.scatter(recoveries_lost.x, recoveries_lost.y, color='green', s=7, marker="o", ax=ax, zorder=2,
                           label="Recovery failed")
 
-    ax.legend(loc='lower center', ncol=2, fontsize=7, bbox_to_anchor=(0.5, -0.05))
-
+    ax.legend(loc='lower center', ncol=2, fontsize=8, bbox_to_anchor=(0.5, -0.07))
     return
 
 
@@ -128,7 +117,7 @@ def shot_pitch(ax, player):
     # creating pitch
     pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black', linewidth=1)
     pitch.draw(ax=ax, figsize=(16, 16))
-    ax_title = ax.set_title('Shots', fontsize=8)
+    ax_title = ax.set_title('Shots', fontsize=10)
     if len(shots) > 0:
         shots[['x', 'y']] = shots['location'].apply(pd.Series)
         shots_goal = shots[(shots.shot_outcome == "Goal")]
@@ -148,8 +137,7 @@ def shot_pitch(ax, player):
             pitch.arrows(shots_goal.x, shots_goal.y, shots_goal.shot_end_x, shots_goal.shot_end_y,
                          width=1, headwidth=4, headlength=3, color='green', ax=ax, zorder=2, label="Goal")
 
-    ax.legend(loc='lower center', ncol=3, fontsize=7, bbox_to_anchor=(0.5, -0.05))
-
+    ax.legend(loc='lower center', ncol=3, fontsize=8, bbox_to_anchor=(0.5, -0.07))
     return
 
 
@@ -158,7 +146,6 @@ def empty_pitch(ax):
     # creating pitch
     pitch = Pitch(pitch_type='statsbomb', pitch_color='white', line_zorder=2, line_color='black', linewidth=1)
     pitch.draw(ax=ax, figsize=(16, 11))
-
     return
 
 
@@ -174,7 +161,7 @@ def prepare_figure(player, position):
     plt.subplots_adjust(left=0.0, bottom=0.1, right=1.0, top=0.9, wspace=0.1, hspace=0.1)
 
     fig.set_facecolor('white')
-    fig.suptitle(f'{player} game performance')
+    # fig.suptitle(f'{player} game performance')
 
     if position == 'Goalkeeper':
         pass_pitch(axs[0], player)
@@ -185,7 +172,6 @@ def prepare_figure(player, position):
         duel_pitch(axs[1, 0], player)
         shot_pitch(axs[1, 1], player)
         # empty_pitch(axs[2, 1])
-
     return fig
 
 
@@ -200,31 +186,31 @@ def creating_stat_table(player, position):
     if position == 'Goalkeeper':
         # number of shots saved and parried to opponent
         saved_op = len(event[(event.player == player) & (event.type == "Goal Keeper") & (
-                    event.goalkeeper_type == "Shot Saved") & (event.goalkeeper_outcome == 'In Play Danger')])
+                event.goalkeeper_type == "Shot Saved") & (event.goalkeeper_outcome == 'In Play Danger')])
         # number of shots saved and parried to a teammate
         saved_t = len(event[(event.player == player) & (event.type == "Goal Keeper") & (
-                    event.goalkeeper_type == "Shot Saved") & (event.goalkeeper_outcome == 'In Play Safe')])
+                event.goalkeeper_type == "Shot Saved") & (event.goalkeeper_outcome == 'In Play Safe')])
         # number of goals conceded
         conceded = len(event[(event.player == player) & (event.type == "Goal Keeper") & (
-                    event.goalkeeper_type == "Goal Conceded")])
+                event.goalkeeper_type == "Goal Conceded")])
         # number of succesful punches
         punches = len(event[(event.player == player) & (event.type == "Goal Keeper") & (
-                    event.goalkeeper_type == "Punches") & (event.goalkeeper_outcome == 'Sucess')])
+                event.goalkeeper_type == "Punches") & (event.goalkeeper_outcome == 'Sucess')])
         names1 = ['success rate of passes', 'succesfull ball recoveries', 'shots saved and parried to opponent']
         stats1 = [passes, rec, saved_op]
         names2 = ['shots saved and parried to a teammate', 'goals conceded', 'succesful punches']
-        stats2 = [ saved_t, conceded, punches]
+        stats2 = [saved_t, conceded, punches]
     else:
         # number of shots on target
         player_s = event[(event.player == player) & (event.type == "Shot")]
         shots = len(player_s[(player_s.shot_outcome == 'Saved') | (player_s.shot_outcome == 'Goal') | (
-                    player_s.shot_outcome == 'Saved To Post')])
+                player_s.shot_outcome == 'Saved To Post')])
         # number of won duels
         player_r = event[(event.player == player) & (event.type == "Duel")]
         duels = len(player_r[(player_r.duel_outcome == 'Won') | (player_r.duel_outcome == 'Success In Play') |
                              (player_r.duel_outcome == 'Success') | (player_r.duel_outcome == 'Success Out') |
                              (player_r.duel_outcome == 'Success To Team') | (
-                                         player_r.duel_outcome == 'Aerial Success')])
+                                     player_r.duel_outcome == 'Aerial Success')])
         # number of succesfull dribligns
         player_dr = event[(event.player == player) & (event.type == "Dribble")]
         drib = len(player_dr[(player_dr.dribble_outcome == 'Complete')])
@@ -238,9 +224,9 @@ def creating_stat_table(player, position):
         names2 = ['succesfull dribblings', 'ball losses', 'fouls commited', 'fouls won']
 
         stats1 = [passes, rec, shots, duels]
-        stats2 = [ drib, los, fc, fw]
-    df1 = pd.DataFrame(list(zip(names1, stats1)), columns=['Parameter', 'Value'])
-    df2 = pd.DataFrame(list(zip(names2, stats2)), columns=['Parameter', 'Value'])
+        stats2 = [drib, los, fc, fw]
+    df1 = pd.DataFrame(list(zip(stats1)), columns=['Value'], index=names1)
+    df2 = pd.DataFrame(list(zip(stats2)), columns=['Value'], index=names2)
     return df1, df2
 
 
@@ -251,52 +237,56 @@ player_position_list = event[(event.player == chosen_player)]['position'].unique
 if len(player_position_list) == 1:
     position = player_position_list[0]
 
-st.header(f'{chosen_player} statistics')
+st.write("\n")
+st.subheader('Statistics', divider=True)
 stats = creating_stat_table(chosen_player, position)
 df1 = stats[0].transpose()
 df2 = stats[1].transpose()
 st.table(df1.style.set_properties(**{'background-color': 'lightblue'}))
 st.table(df2.style.set_properties(**{'background-color': 'lightblue'}))
+st.subheader('Game performance', divider=True)
 st.pyplot(prepare_figure(chosen_player, position))
+
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("data provided by")
+logofile = 'hudlstatsbomblogo.jpg'
+st.image(logofile)
 
 # creating button with on click
 def drawBtn():
-    st.button("Print to pdf", on_click= creating_pdf, use_container_width=True, type = 'primary')
-   
+    st.button("Print to pdf", on_click=creating_pdf, use_container_width=True, type='primary')
+
+
 # creating pdf with report
 def creating_pdf():
     with PdfPages(f'{chosen_player} report.pdf') as pdf:
-        fig, (ax1, ax2) = plt.subplots(2,1,figsize=(8,3))
-        
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 3))
+
         ax1.axis('off')
         ax2.axis('off')
-        
-    
+
         df1 = stats[0].transpose()
         df2 = stats[1].transpose()
-        
-        
-        table1 = ax1.table(cellText = df1.values,
-                          colLabels = df1.columns,
-                          cellColours = [['lightblue']*len(df1.columns),
-                                         ['lightblue']*len(df1.columns)],
-                          loc = 'center')
-    
-        
-        table2 = ax2.table(cellText = df2.values,
-                          colLabels = df2.columns,
-                          cellColours = [['lightblue']*len(df1.columns),
-                                         ['lightblue']*len(df1.columns)],
-                          loc = 'center')
-        
-        
+        print(df2)
+
+        table1 = ax1.table(cellText=df1.values,
+                           colLabels=df1.columns,
+                           cellColours=[['lightblue'] * len(df1.columns)],
+                           loc='center')
+
+        table2 = ax2.table(cellText=df2.values,
+                           colLabels=df2.columns,
+                           cellColours=[['lightblue'] * len(df1.columns)],
+                           loc='center')
+
         fig.suptitle("Player statistics")
         pdf.savefig()
-        
-        fig1 =  prepare_figure(chosen_player, position)
+
+        fig1 = prepare_figure(chosen_player, position)
         fig1.show()
-        
+
         pdf.savefig()
-        
-        
+
 drawBtn()
