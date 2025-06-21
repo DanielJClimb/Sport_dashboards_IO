d4f5e8c830da6bcb09a1a52386ed40a1408606e6
DanielJClimb <110120911+DanielJClimb@users.noreply.github.com>
2025-06-21 16:58:16 +0200

    Update opening_streamlit.py
    Resolved error connected with app trying to run on used port

--- opening_streamlit.py
+++ opening_streamlit.py
@@ -5,4 +5,5 @@ Created on Thu Dec  5 14:59:06 2024
 
 import os
 print('Please wait, while SCOS app is opening')
-os.system('streamlit run 'r'titlepage.py')
+os.system('streamlit run 'r'titlepage.py --server.port 8502')
+
